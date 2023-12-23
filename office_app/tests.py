d0afb31188de38ea5d5491ca1070daa528aac533
Joonatan Talviste <jtalviste@gmail.com>
2023-12-23 04:40:37 +0200

    app inited, example testcase

---
+++ office_app/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from django.test import TestCase
+
+"""
+class ProductListViewTests(TestCase):
+
+    def test_product_list_view(self):
+        # Create a product
+        product = Product.objects.create(name='Laptop', price=1000)
+
+        # Create a request object
+        request = self.client.get('/products/')
+
+        # Get the response
+        response = product_list(request)
+
+        # Check the response status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check the response content
+        self.assertContains(response, product.name)
+        self.assertContains(response, str(product.price))
+"""
+print("TODO: delete")
+
--- trans_connetor_task/urls.py
+++ trans_connetor_task/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from trans_connector_app import views
+from office_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
