5054557f42703383442bfeaf7e210375dfb72fcd
Joonatan Talviste <jtalviste@gmail.com>
2023-12-23 05:45:24 +0200

    added test TODOs and models

--- office_app/models.py
+++ office_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Office(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    address = models.TextField()
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4)
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class WorkHistory(models.Model):
+    office = models.ForeignKey(Office, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    last_checked = models.DateTimeField()
+
--- office_app/tests.py
+++ office_app/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-
-from django.test import TestCase
+from .models import Office
+from .models import Person
+from .models import WorkHistory
 
 """
 class ProductListViewTests(TestCase):
@@ -23,4 +24,28 @@ class ProductListViewTests(TestCase):
         self.assertContains(response, str(product.price))
 """
 print("TODO: delete")
+
+# create multiple methods in a testcase if they share setup and teardown
+class XyzViewXyTestCase(TestCase):
+
+    def test_product_list_view(self):
+        print("TODO: delete")
+
+print("TODO: # test fixtures have data")
+print("TODO: # test that sample API key is loaded from ENV variables")
+print("TODO: check that can access https://open-meteo.com/ API")
+print("TODO: # Get office info by name and current temperature at this location at the moment")
+print("TODO: # Get employees in an office")
+print("TODO: # Get employees by first name")
+print("TODO: # Get employees by last name")
+print("TODO: # Get employees by first name and last name")
+print("TODO: # Get employees returns the office where they work and places where they have worked previously")
+print("TODO: # Add new employee to an office")
+print("TODO: # Update employee’s offices which they have worked at")
+print("TODO: # scheduled task to runs every day")
+print("TODO: # scheduled task updates the last_checked field with the date on which the task is running")
+print("TODO: # scheduled task can be run in batches iterated")
+
+print("TODO: dockerise")
+
 
